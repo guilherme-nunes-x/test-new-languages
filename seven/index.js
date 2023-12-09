02c9e62f4bba9d0c6bc6ba886ede3f6a7135df47
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-12-08 21:45:43 -0300

    Add files via upload

---
+++ seven/index.js
@@ -0,0 +1,15 @@
+//estudando javascript
+console.log("isso é um console.log");
+/*
+formas de determinar variaveis em js
+-> let
+-> var
+-> const
+*/
+let a, b, c;
+a = 1;
+b = 3;
+c = a - b;
+console.log(c);
+//como exprimir mensagem no html
+document.getElementById("nunes").innerHTML = c;
