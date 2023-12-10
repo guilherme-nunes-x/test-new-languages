e8c042f52f8463a0f382b01b671eefe006bfe3f0
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-12-10 10:01:58 -0300

    Add files via upload

--- seven/index.js
+++ seven/index.js
@@ -13,3 +13,20 @@ c = a - b;
 console.log(c);
 //como exprimir mensagem no html
 document.getElementById("nunes").innerHTML = c;
+
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 15;
+total = valor1 + valor2;
+//alert(total);
+
+var nota1, nota2, nota3, media, total, resultado;
+
+nota1 = 4;
+nota2 = 3;
+nota3 = 3;
+total = nota1 + nota2 + nota3;
+media = total / 3;
+//alert("sua media foi igual " + media);
+resultado = media >= 3 ? "voçe está na media" : "voçe não estar na media";
+alert(resultado);
