fccc6520b8a3754d5fd0623ed1de577b4af41cac
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-12-15 21:03:46 -0300

    Add files via upload

---
+++ resultado.py
@@ -0,0 +1,7 @@
+magua = float(input("Digite a massa da agua em quilos:"))
+cagua = float(input("Digite o calor especifico da agua:"))
+variacao = float(input("Digite a variacao de temperatura:"))
+
+kcal = magua*cagua*variacao
+
+print("A quantidade de calor retirado da agua deve ser de: " + str(kcal) +"kcal" )
