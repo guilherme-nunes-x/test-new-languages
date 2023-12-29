5abd1923650af50d5695bb381400d96cefe943eb
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-12-29 12:40:58 -0300

    Delete my_test directory

--- my_test/unit.rb
+++
@@ -1,9 +0,0 @@
-puts "digite seu nome"
-nome = gets.chomp
-puts"digite sua idade"
-idade = gets.chop.to_i
-
-
-puts "hello world"
-puts "my name is #{nome}"
-print "my name is #{nome} e minha idade #{idade}"
