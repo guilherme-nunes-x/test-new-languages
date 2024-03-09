0b730e92aee7ee682b0d22cd4ced4b028178a15b
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-03-09 20:38:57 -0300

    Add files via upload

---
+++ main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hello wold")
+
+	//strings = textos
+
+	var name1 string = "jonh"
+	var name2 = "fernando"
+
+	fmt.Println(name1, name2)
+
+	name1 = "fernando"
+	name2 = "jonh"
+
+	fmt.Println(name1, name2)
+
+	name3 := "estevã"
+
+	fmt.Println(name3)
+
+	//Inteiros = int
+
+	var number int = 23
+	var number2 int = 24
+	var soma = 23 + 24
+
+	fmt.Println(number, number2, soma)
+}
