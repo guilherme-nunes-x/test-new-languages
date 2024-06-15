f03302abc596ec25f8bd3be22e68a07fb3d83f80
guilherme-nunes-x <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-06-15 20:21:27 -0300

    Mais conhecimentos de javascript

---
+++ Estudando_javascript/js/no.js
@@ -0,0 +1,5 @@
+export const number = 12;
+export function addc(number1, number2) {
+
+    return number1 + number2;
+}
---
+++ Estudando_javascript/js/script.js
@@ -0,0 +1,33 @@
+//Formas de imprimir mensagens com javascript
+console.log("Hello wolrd");
+var name = "Guilherme"; //Variavel determinada
+document.getElementById("name").innerHTML = name
+// alert("error")
+
+// Declarações
+
+let a, b, c;
+a = 1;
+b = 2;
+c = 3;
+
+name = "Guilherme"; //Variavel dinamica
+name = "Luis"; //Reatribuição
+
+function myfunction() {
+name = "Guilherme";
+    console.log(name)
+}
+myfunction();
+
+import {addc, number } from '/js/no.js';
+
+console.log(addc(number,23));
+
+// export const EXPECTED_MINUTES_IN_OVEN = 40;
+
+// function remainingMinutesInOven(num) {
+//  return EXPECTED_MINUTES_IN_OVEN - num; 
+// }
+
+// console.log(remainingMinutesInOven(20)); 
