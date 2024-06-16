5bd10884f32f78512274ea8835a9f9f52143c30b
guilherme-nunes-x <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-06-15 21:16:32 -0300

    Complementando conhecimentos

--- Estudando_javascript/js/script.js
+++ Estudando_javascript/js/script.js
@@ -1,7 +1,7 @@
 //Formas de imprimir mensagens com javascript
 console.log("Hello wolrd");
 var name = "Guilherme"; //Variavel determinada
-document.getElementById("name").innerHTML = name
+document.getElementById("name").innerHTML = name;
 // alert("error")
 
 // Declarações
@@ -14,20 +14,69 @@ c = 3;
 name = "Guilherme"; //Variavel dinamica
 name = "Luis"; //Reatribuição
 
-function myfunction() {
-name = "Guilherme";
-    console.log(name)
+export function myFunction() {
+  alert("Hello people");
 }
-myfunction();
 
-import {addc, number } from '/js/no.js';
+import { addc, number } from "/js/no.js";
 
-console.log(addc(number,23));
+console.log(addc(number, 23));
 
 // export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 // function remainingMinutesInOven(num) {
-//  return EXPECTED_MINUTES_IN_OVEN - num; 
+//  return EXPECTED_MINUTES_IN_OVEN - num;
 // }
 
-// console.log(remainingMinutesInOven(20)); 
+// console.log(remainingMinutesInOven(20));
+
+var number1, number2;
+
+number1 = 12;
+number2 = 23;
+let resul;
+resul = number < 18 ? "vc é menor de idade" : "Vc é maior de idade";
+
+console.log(resul);
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+let Variavel = add(12, 43);
+alert(Variavel);
+
+// Operadores de atribuição
+// soma
+// resul = number1 + number2;
+// console.log(resul);
+// number1 += number2;
+// console.log(number1);
+// number1 = number1 + number2;
+// console.log(number1);
+// number1 = ++number1;
+// console.log(number1);
+
+// // divisão
+// resul = number1 / number2;
+// console.log(resul);
+// number1 /= number2;
+// console.log(number1);
+
+
+// // subtração
+// resul = number1 - number2;
+// console.log(resul);
+// number1 -= number2;
+// console.log(number1);
+
+// // multiplicação
+// resul = number1 * number2;
+// console.log(resul);
+// number1 *= number2;
+// console.log(number1);
+
+//operadores em strings
+let nome = "Guilherme";
+let sobrenome = "Luis";
+let nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
