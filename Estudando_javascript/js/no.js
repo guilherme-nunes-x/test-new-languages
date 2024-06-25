a97f89e59a49e19749e43be13586f5e92ead1bb6
guilherme-nunes-x <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-06-24 23:11:06 -0300

    Novo estudo

--- Estudando_javascript/js/no.js
+++ Estudando_javascript/js/no.js
@@ -3,3 +3,10 @@ export function addc(number1, number2) {
 
     return number1 + number2;
 }
+
+let names = ["John", "Paul", "George", "Ringo"];
+
+for (let i = 0; i < count(names).length; i++) {
+ console.log(names[i]);
+}
+
